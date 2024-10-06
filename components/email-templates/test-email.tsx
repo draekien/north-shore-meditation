201a3cfe8426d5bcb494ab9ecd917ba2de1dbef3
William Pei <draekien@users.noreply.github.com>
2024-10-07 00:48:37 +1100

    feat: resend api key

---
+++ components/email-templates/test-email.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface EmailTemplateProps {
+  firstName: string;
+}
+
+export const TestTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ firstName }) => (
+  <div>
+    <h1>Welcome, {firstName}!</h1>
+  </div>
+);
