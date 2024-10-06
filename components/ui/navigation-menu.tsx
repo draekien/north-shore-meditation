e57f759ddb0442217aa4d4bd1f1fd05d09681994
William Pei <draekien@users.noreply.github.com>
2024-10-06 23:07:09 +1100

    feat: mobile menu

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -130,7 +130,29 @@ const NavigationMenuListItem = React.forwardRef<React.ElementRef<'a'>, React.Com
 );
 NavigationMenuListItem.displayName = 'NavigationMenuListItem';
 
+const MobileNavigationMenuListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
+        </a>
+      </li>
+    );
+  }
+);
+MobileNavigationMenuListItem.displayName = 'MobileNavigationMenuListItem';
+
 export {
+  MobileNavigationMenuListItem,
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuIndicator,
@@ -142,3 +164,4 @@ export {
   navigationMenuTriggerStyle,
   NavigationMenuViewport,
 };
+
