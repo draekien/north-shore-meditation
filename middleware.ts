e5cee262a6be03eaf0967268078298e55daaf803
william pei <draekien@users.noreply.github.com>
2024-10-07 16:33:10 +1100

    feat: blur background image

--- middleware.ts
+++ middleware.ts
@@ -27,7 +27,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
-    '/((?!_next).*)',
+    '/((?!_next|favicon|icon|apple-icon).*)',
     // Optional: only run on root (/) URL
     // '/'
   ],
