471a9aebb531cbc3890d52f5edc3d42a577510c5
William Pei <draekien@users.noreply.github.com>
2024-10-11 23:27:33 +1100

    feat: remove front on image

--- app/[lang]/about-us/page.tsx
+++ app/[lang]/about-us/page.tsx
@@ -5,7 +5,6 @@ import PrimaryPageSection from '@/components/ui/page-section.primary';
 import SecondaryPageSection from '@/components/ui/page-section.secondary';
 import type { GlobalPageProps } from '@/lib/types';
 import graceAbout from '@/public/grace_about.jpg';
-import graceHappy from '@/public/grace_happy.jpg';
 import Image from 'next/image';
 import { getDictionary } from '../dictionaries';
 
@@ -18,13 +17,10 @@ export default async function AboutUsPage({ params: { lang } }: GlobalPageProps)
     <PageContent>
       <PrimaryPageSection>
         <PageSectionContainer>
-          <div className="flex flex-col items-center justify-around md:min-h-[50dvh] md:flex-row">
+          <div className="flex flex-col items-center md:min-h-[50dvh] md:flex-row">
             <div className="md:w-1/2">
               <h1 className="text-primary">{aboutUs.title}</h1>
               <p className="mb-6 text-xl text-foreground">{aboutUs.subtitle}</p>
-            </div>
-            <div className="rounded-xl shadow-lg md:w-1/3">
-              <Image src={graceAbout} alt={aboutUs.heroAlt} placeholder="blur" className="rounded-xl" />
             </div>
           </div>
         </PageSectionContainer>
@@ -32,14 +28,14 @@ export default async function AboutUsPage({ params: { lang } }: GlobalPageProps)
       <SecondaryPageSection>
         <PageSectionContainer>
           <div className="flex flex-col items-center justify-around md:min-h-[50dvh] md:flex-row">
-            <div className="rounded-xl shadow-lg md:w-1/3">
-              <Image src={graceHappy} alt={aboutUs.sections.grace.imageAlt} placeholder="blur" className="rounded-xl" />
-            </div>
             <div className="md:w-1/2">
               <h2 className="text-primary">{aboutUs.sections.grace.title}</h2>
               {aboutUs.sections.grace.paragraphs.map((text, index) => (
                 <p key={index}>{text}</p>
               ))}
+            </div>
+            <div className="rounded-xl shadow-lg md:w-1/3">
+              <Image src={graceAbout} alt={aboutUs.heroAlt} placeholder="blur" className="rounded-xl" />
             </div>
           </div>
         </PageSectionContainer>
