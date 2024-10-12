57be11b519762b65bd1476bbe82a76b31630b210
William Pei <draekien@users.noreply.github.com>
2024-10-12 22:59:44 +1100

    feat: enable vercel toolbar in localhost

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+import VercelToolbar from '@vercel/toolbar/plugins/next';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withVercelToolbar = VercelToolbar();
+
+export default withVercelToolbar(nextConfig);
