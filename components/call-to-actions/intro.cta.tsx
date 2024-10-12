c5e12ea265ab50d82b71cb565f02fd7f57dbced2
William Pei <draekien@users.noreply.github.com>
2024-10-12 21:13:42 +1100

    feat: watch video

--- components/call-to-actions/intro.cta.tsx
+++ components/call-to-actions/intro.cta.tsx
@@ -1,10 +1,9 @@
 import { cn } from '@/lib/utils';
 import searching from '@/public/undraw_searching_re_3ra9.svg';
-import { DialogDescription } from '@radix-ui/react-dialog';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '../ui/button';
-import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '../ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
 import type { CallToAction } from './types';
 
 export type IntroCallToAction = {
@@ -33,28 +32,37 @@ export default function IntroCallToActionSection({
             <Image src={searching} alt="" height={300} />
           </div>
           <div className="md:w-1/2">
-            <p className="my-4 text-xl text-foreground">{subtitle}</p>
-            <div>
+            <p className="my-4 text-xl text-foreground">
+              {subtitle}
+              <Dialog>
+                <DialogTrigger>
+                  <Button variant="link" className="px-1 text-lg">
+                    {dialogTrigger}
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>{dialogTitle}</DialogTitle>
+                    <DialogDescription>{dialogDescription}</DialogDescription>
+                  </DialogHeader>
+                  <iframe
+                    className="aspect-video w-full rounded-md border-0"
+                    src="https://www.youtube-nocookie.com/embed/fHBUjQCIiqg?si=uBj1TNDTPR7Xgcuk"
+                    title={dialogTitle}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                    loading="lazy"
+                  ></iframe>
+                </DialogContent>
+              </Dialog>
+            </p>
+            <div className="flex flex-row gap-4 align-middle">
               <Link href={href} referrerPolicy="no-referrer">
                 <Button className="px-8 py-3 text-lg font-semibold" size="lg" variant="secondary">
                   {action}
                 </Button>
               </Link>
-              <Dialog>
-                <DialogTrigger>{dialogTrigger}</DialogTrigger>
-                <DialogContent>
-                  <DialogTitle>{dialogTitle}</DialogTitle>
-                  <DialogDescription>{dialogDescription}</DialogDescription>
-                  <iframe
-                    className="aspect-video w-full"
-                    src="https://www.youtube-nocookie.com/embed/fHBUjQCIiqg?si=uBj1TNDTPR7Xgcuk"
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen
-                  ></iframe>
-                </DialogContent>
-              </Dialog>
             </div>
             <div className="mt-16 text-sm">
               <small>{helpText}</small>
