49fba884f7b7b5d8bad78af49824dcf9279a6137
William Pei <draekien@users.noreply.github.com>
2024-10-13 19:12:07 +1100

    feat: add robots

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: ['/', '/_next/static/media', '/_next/static/css', '/_next/static/chunks'],
+      disallow: ['/api/', '/public/', '/_next/', '/404.html'],
+    },
+    sitemap: 'https://www.northshoremeditation.au/sitemap.xml',
+  };
+}
