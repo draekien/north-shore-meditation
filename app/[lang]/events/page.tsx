da44338f3794a4182849c58128d0c16e99b63848
William Pei <draekien@users.noreply.github.com>
2026-01-02 13:30:09 +1100

    feat: page layout stuff for events

--- app/[lang]/events/page.tsx
+++ app/[lang]/events/page.tsx
@@ -9,7 +9,6 @@ import { Empty, EmptyHeader, EmptyMedia, EmptyTitle, EmptyDescription } from '@/
 import { CalendarClockIcon, CalendarSyncIcon, MapPinIcon, UsersIcon } from 'lucide-react';
 import {
   ItemGroup,
-  ItemHeader,
   ItemDescription,
   Item,
   ItemContent,
@@ -18,9 +17,12 @@ import {
   ItemMedia,
   ItemFooter,
 } from '@/components/ui/item';
-import { CalendarIcon, ExternalLinkIcon } from '@radix-ui/react-icons';
+import { ExternalLinkIcon } from '@radix-ui/react-icons';
 import { Badge } from '@/components/ui/badge';
-import { Separator } from '@/components/ui/separator';
+import PageContent from '@/components/ui/page-content';
+import PrimaryPageSection from '@/components/ui/page-section.primary';
+import SecondaryPageSection from '@/components/ui/page-section.secondary';
+import PageSectionContainer from '@/components/ui/page-section.container';
 
 export async function generateMetadata(props: GlobalPageProps): Promise<Metadata> {
   const { lang } = await props.params;
@@ -85,62 +87,103 @@ export default async function Page({ searchParams, params }: GlobalPageProps) {
   }
 
   return (
-    <ItemGroup className="container m-4">
-      {items.map((item) => {
-        const startsAt = new Date(item.startsAt);
-        const endsAt = new Date(item.endsAt);
+    <PageContent>
+      <PrimaryPageSection>
+        <PageSectionContainer>
+          <div className="flex flex-col items-center md:min-h-[50dvh] md:flex-row">
+            <div className="mb-8 md:mb-0 md:w-1/2">
+              <h1 className="text-primary">Upcoming Events</h1>
+              <p className="mb-6 text-xl text-foreground">
+                Our events are designed to meet you where you are. Whether you’re attending an intro session, committing
+                to a course, or stepping into a retreat, each event supports effortless meditation and lasting change.
+              </p>
+            </div>
+          </div>
+        </PageSectionContainer>
+      </PrimaryPageSection>
+      <SecondaryPageSection>
+        <PageSectionContainer>
+          {!items.length ? (
+            <Empty>
+              <EmptyHeader>
+                <EmptyMedia variant="icon">
+                  <CalendarSyncIcon />
+                </EmptyMedia>
+                <EmptyTitle>Taking a Short Breather</EmptyTitle>
+                <EmptyDescription>
+                  We&apos;re between events at the moment. New sessions and programs are being planned, so check back
+                  soon or join our mailing list for updates.
+                </EmptyDescription>
+              </EmptyHeader>
+            </Empty>
+          ) : (
+            <ItemGroup className="container m-4">
+              {items.map((item) => {
+                const startsAt = new Date(item.startsAt);
+                const endsAt = new Date(item.endsAt);
 
-        return (
-          <Item key={item.sys.id} variant="muted" asChild>
-            <a href={item.bookingUrl!} target="_blank" rel="noopener noreferrer">
-              <ItemContent>
-                <ItemTitle>
-                  <Badge variant="outline">{item.type}</Badge> {item.name}
-                </ItemTitle>
-                {item.description && <ItemDescription>{item.description}</ItemDescription>}
-                <ItemGroup>
-                  <Item size="sm">
-                    <ItemMedia>
-                      <CalendarClockIcon className="size-4" />
-                    </ItemMedia>
-                    <ItemContent>
-                      <ItemTitle>
-                        {startsAt.toLocaleString()} - {endsAt.toLocaleString()}
-                      </ItemTitle>
-                    </ItemContent>
+                return (
+                  <Item key={item.sys.id} variant="outline" asChild>
+                    <a href={item.bookingUrl!} target="_blank" rel="noopener noreferrer">
+                      <ItemMedia>
+                        <Badge>{item.type}</Badge>
+                      </ItemMedia>
+                      <ItemContent>
+                        <ItemTitle>{item.name}</ItemTitle>
+                        {item.description && <ItemDescription>{item.description}</ItemDescription>}
+                      </ItemContent>
+                      <ItemActions>
+                        <ExternalLinkIcon />
+                      </ItemActions>
+                      <ItemFooter>
+                        <ItemGroup>
+                          <Item size="sm">
+                            <ItemMedia>
+                              <UsersIcon className="size-4" />
+                            </ItemMedia>
+                            <ItemContent>
+                              <ItemTitle>
+                                <div className="flex gap-2">
+                                  {item.audiences?.map((audience) => (
+                                    <Badge key={audience} variant="outline">
+                                      {audience}
+                                    </Badge>
+                                  ))}
+                                </div>
+                              </ItemTitle>
+                            </ItemContent>
+                          </Item>
+                          <Item size="sm">
+                            <ItemMedia>
+                              <CalendarClockIcon className="size-4" />
+                            </ItemMedia>
+                            <ItemContent>
+                              <ItemTitle>
+                                {startsAt.toLocaleString()} - {endsAt.toLocaleString()}
+                              </ItemTitle>
+                            </ItemContent>
+                          </Item>
+                          <Item size="sm">
+                            <ItemMedia>
+                              <MapPinIcon className="size-4" />
+                            </ItemMedia>
+                            <ItemContent>
+                              <ItemTitle>{item.location}</ItemTitle>
+                              {item.locationDescription && (
+                                <ItemDescription>{item.locationDescription}</ItemDescription>
+                              )}
+                            </ItemContent>
+                          </Item>
+                        </ItemGroup>
+                      </ItemFooter>
+                    </a>
                   </Item>
-                  <Item size="sm">
-                    <ItemMedia>
-                      <MapPinIcon className="size-4" />
-                    </ItemMedia>
-                    <ItemContent>
-                      <ItemTitle>{item.location}</ItemTitle>
-                      {item.locationDescription && <ItemDescription>{item.locationDescription}</ItemDescription>}
-                    </ItemContent>
-                  </Item>
-                  <Item size="sm">
-                    <ItemMedia>
-                      <UsersIcon className="size-4" />
-                    </ItemMedia>
-                    <ItemContent>
-                      <ItemTitle>
-                        <div className="flex gap-2">
-                          {item.audiences?.map((audience) => (
-                            <Badge key={audience}>{audience}</Badge>
-                          ))}
-                        </div>
-                      </ItemTitle>
-                    </ItemContent>
-                  </Item>
-                </ItemGroup>
-              </ItemContent>
-              <ItemActions>
-                <ExternalLinkIcon />
-              </ItemActions>
-            </a>
-          </Item>
-        );
-      })}
-    </ItemGroup>
+                );
+              })}
+            </ItemGroup>
+          )}
+        </PageSectionContainer>
+      </SecondaryPageSection>
+    </PageContent>
   );
 }
